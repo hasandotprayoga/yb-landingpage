984cb4a2ba7651e5e5b1026fe583de38d54593ff
Hasan Prayoga <hasandotprayoga@gmail.com>
2023-03-07 06:17:45 +0700

    first commit

---
+++ _nuxt/default.db568209.js
@@ -0,0 +1,1 @@
+import{a as r,v as t}from"./entry.21e9f21f.js";const s={};function a(e,n){return t(e.$slots,"default")}const c=r(s,[["render",a]]);export{c as default};
---
+++ _nuxt/nuxt-icon.81bbdf28.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./nuxt-icon.vue.c9ebcbc9.js";import"./entry.21e9f21f.js";export{m as default};
